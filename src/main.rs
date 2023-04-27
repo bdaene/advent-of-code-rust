9140526ff92a7966affccc80bfa0fb2d8ddaf3a3
Benoît Daene <benoitdaene@gmail.com>
2023-04-27 16:40:46 +0200

    Add timing.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::time;
 
 use advent_of_code_2022::get_solution;
 use clap::Parser;
@@ -16,8 +17,15 @@ fn main() {
 
     let data = fs::read_to_string(args.data).expect("Input data not found.");
 
+    let start = time::Instant::now();
     let solution = get_solution(args.day, &data);
+    println!("Parsed data in {:?}", start.elapsed());
 
-    println!("Part 1: {}", solution.part_1());
-    println!("Part 2: {}", solution.part_2());
+    let start = time::Instant::now();
+    let answer = solution.part_1();
+    println!("Part 1 ({:?}): {}", start.elapsed(), answer);
+
+    let start = time::Instant::now();
+    let answer = solution.part_2();
+    println!("Part 2 ({:?}): {}", start.elapsed(), answer);
 }
